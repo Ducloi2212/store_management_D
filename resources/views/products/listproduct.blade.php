1fb41f1f52afc42cbcc587110a96e5c9421f3f80
DDLoi <dinhducloi477@gmail.com>
2025-05-14 17:50:46 +0700

    update show all products

--- resources/views/products/listproduct.blade.php
+++ resources/views/products/listproduct.blade.php
@@ -1,13 +1,15 @@
 <section class="container mt-4">
     <h4>IPHONE</h4>
     <div class="row">
+
+        @foreach($products as $product)
         <div class="col-md-3 mb-4">
             <div class="card shadow-sm h-100 card-custom-rounded product-card">
-                
-                <img src="{{ asset('images/products/phone/ip14.jpg') }}" class="card-img-top" alt="iPhone 16 Pro Max">
+
+                <img src="{{ asset($product->image) }}" class="card-img-top" alt="image">
                 <div class="card-body">
-                    <h6 class="card-title">iPhone 16 Pro Max 256GB - Chính Hãng VN/A</h6>
-                    <p class="text-danger fw-bold mb-1">27.999.000₫</p>
+                    <h6 class="card-title">{{ $product->name }}</h6>
+                    <p class="text-danger fw-bold mb-1">{{ $product->price }} ₫</p>
                     <div class="text-warning mb-1">
                         ★★★★★ (0 đánh giá)
                     </div>
@@ -15,34 +17,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-3 mb-4">
-            <div class="card shadow-sm h-100 card-custom-rounded product-card">
-                
-                <img src="{{ asset('images/products/phone/ip15.jpg') }}" class="card-img-top" alt="iPhone 16 Pro Max">
-                <div class="card-body">
-                    <h6 class="card-title">iPhone 16 Pro Max 256GB - Chính Hãng VN/A</h6>
-                    <p class="text-danger fw-bold mb-1">27.999.000₫</p>
-                    <div class="text-warning mb-1">
-                        ★★★★★ (0 đánh giá)
-                    </div>
-                    <span class="badge bg-secondary">Trả góp 0%</span>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <div class="card shadow-sm h-100 card-custom-rounded product-card">
-                
-                <img src="{{ asset('images/products/phone/ip16.jpg') }}" class="card-img-top" alt="iPhone 16 Pro Max">
-                <div class="card-body">
-                    <h6 class="card-title">iPhone 16 Pro Max 256GB - Chính Hãng VN/A</h6>
-                    <p class="text-danger fw-bold mb-1">27.999.000₫</p>
-                    <div class="text-warning mb-1">
-                        ★★★★★ (0 đánh giá)
-                    </div>
-                    <span class="badge bg-secondary">Trả góp 0%</span>
-                </div>
-            </div>
-        </div>
-        
+        @endforeach
+
     </div>
 </section>
