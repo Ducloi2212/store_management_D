59bbf110c8b5f75579d820085177e4d5706b4f09
HuynhPhuoc <huynhphuoc2842@gmail.com>
2025-05-14 20:34:54 +0700

    update nabar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -26,7 +26,7 @@
                     </li>
                     @guest
                     <li class="nav-item">
-                        <a class="nav-link" href="">Login</a>
+                        <a class="nav-link" href="{{ route ('login') }}">Login</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Sign Up</a>
