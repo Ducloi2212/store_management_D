ba2ba06363ac5576f35b8ad7e7caf4fdb8e27763
HuynhPhuoc <huynhphuoc2842@gmail.com>
2025-05-14 23:34:56 +0700

    DDL detail product

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/home', [HomeController::class, 'home'])->name('home');
 
 Route::get('/category', [CategoryController::class, 'categoryProduct'])->name('category.product');
+
+Route::get('/product', [ProductController::class, 'productDetail'])->name('product.detail');
 
 Route::get('/', function () {
     return view('welcome');
