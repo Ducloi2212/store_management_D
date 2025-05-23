624af99024ccd7d41f124229f8857bd9e4f9aed7
HuynhPhuoc <huynhphuoc2842@gmail.com>
2025-05-23 21:04:11 +0700

    Categories admin

---
+++ resources/views/admin/categories/categoryList.blade.php
@@ -0,0 +1,43 @@
+<h1>Danh Mục</h1>
+<a href="{{ route('admin.categories.create') }}" class="btn btn-primary mb-3">Thêm danh mục mới</a>
+
+@if(session('success'))
+<div class="alert alert-success">{{ session('success') }}</div>
+@endif
+
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">
+                <h5>Danh Mục</h5>
+            </th>
+            <th scope="col">
+                <h5>Trạng thái</h5>
+            </th>
+            <th scope="col">
+                <h5>Action</h5>
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($categories as $category)
+        <tr>
+            <th>
+                <h5 class="text-dark">{{ $category->name }}</h5>
+            </th>
+            <td>{{ $category-> status }}</td>
+            <td>
+                <form action="{{ route('admin.categories.destroy', $category->id) }}" method="POST"
+                    onsubmit="return confirm('Bạn có chắc muốn xóa danh mục này không?');">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-light"><i class='bx bxs-trash text-danger'></i></button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+<div class="pagination-wrapper">
+    {{ $categories->appends(request()->all())->links('pagination::bootstrap-5') }}
+</div>
