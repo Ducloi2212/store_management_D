51dfe13b971a73e8455ea468152db38a305e6964
HuynhPhuoc <huynhphuoc2842@gmail.com>
2025-05-31 01:18:21 +0700

    fix input phone

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Trang không tồn tại')
 
 @section('content')
-    <div class="text-center py-5">
-        <h1 class="display-4"></h1>
-        <p class="lead">Rất tiếc, trang bạn yêu cầu không tồn tại.</p>
-        <a href="{{ route('home') }}" class="btn color " style="border: 1px solid orange">Quay lại trang chủ</a>
+    <div class="container">
+        <h1 class="">Trang không tồn tại</h1>
+        <p>trang truy cập không tồn tại!</p>
+        <a href="{{ route('home') }}"class="btn color " style="border: 1px solid orange">Quay lại trang chủ</a>
     </div>
     @endsection
